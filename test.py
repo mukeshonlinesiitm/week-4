9b7f42d92b6a0393617cb3c3db69caf2f16f55d9
mukeshonlinesiitm <21f1000350@ds.study.iitm.ac.in>
2025-06-28 16:15:33 +0000

    test code updated in main branch

--- test.py
+++ test.py
@@ -20,6 +20,15 @@ class TestModel(unittest.TestCase):
         prediction = predicted_class[0]
         print("Predicted class:", predicted_class[0])
         self.assertEqual(prediction,"setosa","Predicted class is wrong")
-        
+
+    def test_sample2(self):
+        sample = pd.DataFrame([[4.6,3.1,1.5,0.2]],
+                         columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+        # Predict the class
+        predicted_class = self.restored_model.predict(sample)
+        prediction = predicted_class[0]
+        print("Predicted class:", predicted_class[0])
+        self.assertEqual(prediction,"setosa","Predicted class is wrong")
+
 if __name__ == '__main__':
     unittest.main()    
