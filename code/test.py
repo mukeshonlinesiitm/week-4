81791241d24a9a81116300061fe7d06ba82502ef
mukeshonlinesiitm <21f1000350@ds.study.iitm.ac.in>
2025-06-28 15:41:51 +0000

    test code update

--- code/test.py
+++ code/test.py
@@ -21,7 +21,7 @@ class TestModel(unittest.TestCase):
         print("Predicted class:", predicted_class[0])
         self.assertEqual(prediction,"setosa","Predicted class is wrong")
     
-    """
+    
     def test_sample2(self):
         sample = pd.DataFrame([[4.6,3.1,1.5,0.2]],
                          columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
@@ -30,6 +30,6 @@ class TestModel(unittest.TestCase):
         prediction = predicted_class[0]
         print("Predicted class:", predicted_class[0])
         self.assertEqual(prediction,"versicolor","Predicted class is wrong")
-    """    
+        
 if __name__ == '__main__':
     unittest.main()    
