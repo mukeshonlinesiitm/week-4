114bbe966bc09347ade9d6278488f8b050437030
mukeshonlinesiitm <21f1000350@ds.study.iitm.ac.in>
2025-08-05 09:38:47 +0000

    modified

--- code/train.py
+++ code/train.py
@@ -10,16 +10,19 @@ def poison_data(X, poison_level):
     """
     Poison a given percentage of samples in X with random noise.
     """
+    
     X_poisoned = X.copy()
     n_samples = X.shape[0]
-    n_poison = int(poison_level * n_samples)
+    n_poison = int(0.05 * n_samples)
 
     # Random indices to poison
-    poison_indices = np.random.choice(n_samples, n_poison, replace=False)
+    poison_indices = np.random.choice(X_poisoned.index, n_poison, replace=False)
 
+    #print(X_poisoned)
     # Inject noise
     for i in poison_indices:
-        X_poisoned[i] = np.random.uniform(
+        #print(i,X_poisoned.loc[i])
+        X_poisoned.loc[i] = np.random.uniform(
             low=np.min(X, axis=0),
             high=np.max(X, axis=0),
             size=X.shape[1]
@@ -34,24 +37,25 @@ train, test = train_test_split(data, test_size = 0.4, stratify = data['species']
 
 results = []
 
-for level in [0.00, 0.05, 0.10, 0.50]:
-    X_train_poisoned = poison_data(X_train, poison_percentage)
-
+for level in [0.05,0.10,0.25,0.50]:
+    
     X_train = train[['sepal_length','sepal_width','petal_length','petal_width']]
+    X_train_poisoned = poison_data(X_train, level)
     y_train = train.species
+    
     X_test = test[['sepal_length','sepal_width','petal_length','petal_width']]
     y_test = test.species
 
     mod_dt = DecisionTreeClassifier(max_depth = 3, random_state = 1)
-    mod_dt.fit(X_train,y_train)
+    mod_dt.fit(X_train_poisoned,y_train)
     prediction=mod_dt.predict(X_test)
     acc= accuracy_score(prediction,y_test)
 
-    print(f"Poison Level: {int(poison_percentage * 100)}% - Validation Accuracy: {acc:.4f}")
+    print(f"Poison Level: {int(level * 100)}% - Validation Accuracy: {acc:.4f}")
         # Save results
 
     results.append({
-        "Poison_Percentage": int(poison_percentage * 100),
+        "Poison_Percentage": int(level * 100),
         "Validation_Accuracy": round(acc, 4)
     })
 
